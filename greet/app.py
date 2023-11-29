8aeb398e35c3b08d2fce05c90db16fb583d3e203
Carl Molina <carlandrew.molina@gmail.com>
2023-11-29 12:08:53 -0800

    Finished greet

---
+++ greet/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.get('/welcome')
+def welcome():
+    print('this in terminal?')
+    return "welcome"
+
+@app.get('/welcome/home')
+def welcome_home():
+    print('this in terminal?')
+    return 'welcome home'
+
+@app.get('/welcome/back')
+def welcome_back():
+    return 'welcome back'
+
